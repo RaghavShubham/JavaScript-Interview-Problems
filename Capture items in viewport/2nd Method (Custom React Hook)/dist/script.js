6600b776fc4b494205f4bb28da90aabbfc033d4d
Shubham Raghav <shubham3raghav@gmail.com>
2023-06-29 19:21:22 +0530

    small change

--- Capture items in viewport/2nd Method (Custom React Hook)/dist/script.js
+++ Capture items in viewport/2nd Method (Custom React Hook)/dist/script.js
@@ -1,24 +1,24 @@
 import React, {
-useRef,
-useState,
-useEffect } from
-"https://esm.sh/react@18.2.0";
+  useRef,
+  useState,
+  useEffect,
+} from "https://esm.sh/react@18.2.0";
 import ReactDom from "https://esm.sh/react-dom@18.2.0";
 
-const useOnScreen = ref => {
+const useOnScreen = (ref) => {
   const [isIntersecting, setIsIntersecting] = useState(false);
   const observer = new IntersectionObserver(
-  ([element]) => {
-    setIsIntersecting(element.isIntersecting);
-  },
-  { threshold: 1.0 });
-
+    ([element]) => {
+      setIsIntersecting(element.isIntersecting);
+    },
+    { threshold: 1.0 }
+  );
 
   useEffect(() => {
     observer.observe(ref.current);
 
     () => {
-      ovserver.unObserve(ref.current);
+      observer.unObserve(ref.current);
     };
   }, []);
 
@@ -33,21 +33,28 @@ const Element = ({ index }) => {
     console.log(index);
   }
 
-  return /*#__PURE__*/(
-    React.createElement("div", { ref: ref, key: index, className: "block" },
-    index));
-
-
+  return /*#__PURE__*/ React.createElement(
+    "div",
+    { ref: ref, key: index, className: "block" },
+    index
+  );
 };
 
 const App = () => {
   const blocks = [];
 
   for (let i = 0; i < 20; i++) {
-    blocks.push( /*#__PURE__*/React.createElement(Element, { index: i + 1 }));
+    blocks.push(/*#__PURE__*/ React.createElement(Element, { index: i + 1 }));
   }
 
-  return /*#__PURE__*/React.createElement("div", { className: "wrapper" }, blocks);
+  return /*#__PURE__*/ React.createElement(
+    "div",
+    { className: "wrapper" },
+    blocks
+  );
 };
 
-ReactDom.render( /*#__PURE__*/React.createElement(App, null), document.getElementById("root"));
+ReactDom.render(
+  /*#__PURE__*/ React.createElement(App, null),
+  document.getElementById("root")
+);
